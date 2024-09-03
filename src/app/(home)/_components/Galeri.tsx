a4d9732f35aa8d160d440a64a65b6dd0ce8c8f6f
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-09-03 11:08:24 +0700

    feat: add favicon add home file system

---
+++ src/app/(home)/_components/Galeri.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Galeri() {
+  return <div></div>;
+}
---
+++ src/app/(home)/_components/Visi.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Visi() {
+  return <div></div>;
+}
